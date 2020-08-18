6a997b8989f46b89fa5c03a5bef5b62b0eb92a88
Philippe Sabourin <philippe.sabourin@gmail.com>
2020-08-18 14:13:47 -0400

    Finished converting sample to typescript.

--- src/RacingBarChart.tsx
+++ src/RacingBarChart.tsx
@@ -8,60 +8,60 @@ type RacingBarChartProps = {
 }
 
 function RacingBarChart({data}:RacingBarChartProps) {
-	const svgRef = useRef<HTMLElement>(null);
-	const wrapperRef = useRef<HTMLElement>(null);
-	const dimensions = useDimensionObserver(wrapperRef);
+	const svgRef = useRef<SVGSVGElement>(null);
+	const wrapperRef = useRef<HTMLDivElement>(null);
+	const {width,height} = useDimensionObserver(wrapperRef);
 
 	// will be called initially and on every data change
 	useEffect(() => {
-		const svg = select<SVGSVGElement>(svgRef.current);
-		if (!dimensions) return;
+		const svg = select(svgRef.current);
+		if (!width || !height) return;
 
 		// sorting the data
 		data.sort((a, b) => b.value - a.value);
 
-		const yScale = scaleBand()
+		const yScale = scaleBand<number>()
 			.paddingInner(0.1)
 			.domain(data.map((value, index) => index)) // [0,1,2,3,4,5]
-			.range([0, dimensions.height]); // [0, 200]
+			.range([0, height]); // [0, 200]
 
 		const xScale = scaleLinear()
-			.domain([0, max(data, entry => entry.value)]) // [0, 65 (example)]
-			.range([0, dimensions.width]); // [0, 400 (example)]
+			.domain([0, max(data, entry => entry.value) ?? 0]) // [0, 65 (example)]
+			.range([0, width]); // [0, 400 (example)]
 
 		// draw the bars
 		svg
-			.selectAll(".bar")
+			.selectAll<SVGRectElement,Horse>(".bar")
 			.data(data, (entry, index) => entry.name)
 			.join(enter =>
-				enter.append("rect").attr("y", (entry, index) => yScale(index))
+				enter.append("rect").attr("y", (entry, index) => yScale(index) ?? 0)
 			)
 			.attr("fill", entry => entry.color)
 			.attr("class", "bar")
 			.attr("x", 0)
 			.attr("height", yScale.bandwidth())
-			.transition()
+			.transition().duration(500)
 			.attr("width", entry => xScale(entry.value))
-			.attr("y", (entry, index) => yScale(index));
+			.attr("y", (entry, index) => yScale(index) ?? 0);
 
 		// draw the labels
 		svg
-			.selectAll(".label")
+			.selectAll<SVGTextElement, Horse>(".label")
 			.data(data, (entry, index) => entry.name)
 			.join(enter =>
 				enter
 					.append("text")
 					.attr(
 						"y",
-						(entry, index) => yScale(index) + yScale.bandwidth() / 2 + 5
+						(entry, index) => (yScale(index) ?? 0) + yScale.bandwidth() / 2 + 5
 					)
 			)
 			.text(entry => `🐎 ... ${entry.name} (${entry.value} meters)`)
 			.attr("class", "label")
 			.attr("x", 10)
-			.transition()
-			.attr("y", (entry, index) => yScale(index) + yScale.bandwidth() / 2 + 5);
-	}, [data, dimensions]);
+			.transition().duration(500)
+			.attr("y", (entry, index) => (yScale(index) ?? 0) + yScale.bandwidth() / 2 + 5);
+	}, [data, width, height]);
 
 	return (
 		<div ref={wrapperRef} style={{marginBottom: "2rem"}}>
