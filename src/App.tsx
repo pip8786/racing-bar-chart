5f109e2fdd71905495eb70d08d6fa3bf1e932e47
Philippe Sabourin <philippe.sabourin@gmail.com>
2020-08-19 13:59:19 -0400

    Fixed a bug in the forward buttons and made the build use relative paths.

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Icon, {Icons} from "./Icons";
 export type Day = {
 	season: string
 	gameDay: number
-    timestamp: number
+	timestamp: number
 	players: Player[] //sorted list of players by goals
 }
 
@@ -20,55 +20,55 @@ export type Player = {
 }
 
 export type PlayerSeasons = {
-	[playerID: number] : string[]
+	[playerID: number]: string[]
 }
 
 function App() {
 	const [iteration, setIteration] = useState(0);
 	const [start, setStart] = useState(false);
 	const [data] = useState(() => {
-	    let days:Day[] = [];
-	    let currentIndex = -1;
-	    for(let goal of Goals) {
-            let timestamp = new Date(goal.date).getTime();
-            let day = days[currentIndex];
-            if(!day || day.timestamp !== timestamp) {
-            	let gameDay = 1;
-            	if(day) {
-            		day.players.sort((a,b) => b.cumulativeGoals - a.cumulativeGoals);
+		let days: Day[] = [];
+		let currentIndex = -1;
+		for (let goal of Goals) {
+			let timestamp = new Date(goal.date).getTime();
+			let day = days[currentIndex];
+			if (!day || day.timestamp !== timestamp) {
+				let gameDay = 1;
+				if (day) {
+					day.players.sort((a, b) => b.cumulativeGoals - a.cumulativeGoals);
 					gameDay = day.season === goal.season ? day.gameDay + 1 : 1;
 				}
-            	let players = day ? JSON.parse(JSON.stringify(day.players)) : [];
+				let players = day ? JSON.parse(JSON.stringify(day.players)) : [];
 
-            	day = {
+				day = {
 					timestamp,
 					players,
 					season: goal.season,
 					gameDay
 				};
-            	days.push(day);
+				days.push(day);
 				currentIndex++;
 			}
 			let player = day.players.find(value => value.id === goal.player_id);
-            if(!player) {
-            	player = {
-            		id: goal.player_id,
+			if (!player) {
+				player = {
+					id: goal.player_id,
 					name: goal.name,
 					cumulativeGoals: 1
 				};
-            	day.players.push(player);
+				day.players.push(player);
 			} else {
-            	player.cumulativeGoals++;
+				player.cumulativeGoals++;
 			}
-        }
-	    return days;
+		}
+		return days;
 	});
 
 	const [seasons] = useState(() => {
-		const seasons:PlayerSeasons = {};
-		for(let playerSeason of Seasons) {
+		const seasons: PlayerSeasons = {};
+		for (let playerSeason of Seasons) {
 			let playerArray = seasons[playerSeason.playerID];
-			if(!playerArray) {
+			if (!playerArray) {
 				playerArray = [];
 				seasons[playerSeason.playerID] = playerArray;
 			}
@@ -83,34 +83,38 @@ function App() {
 		}
 	}, 550);
 
-  return (
-      <React.Fragment>
-        <h1>Goals Racing Bar Chart</h1>
-        <RacingBarChart days={data} iteration={iteration} playerSeasons={seasons}/>
-		<div className="buttonContainer">
-			<button onClick={() => setIteration(iteration => Math.max(iteration - 10,0))} title="Back 10">
-				<Icon type={Icons.chevronsLeft}/>
-			</button>
-			<button onClick={() => setIteration(iteration => Math.max(iteration - 1,0))} title="Previous">
-				<Icon type={Icons.chevronLeft}/>
-			</button>
-			<button onClick={() => setStart(!start)} title={start ? "Pause" : "Play"}>
-				<Icon type={start ? Icons.pause : Icons.play}/>
-			</button>
-			<button onClick={() => setIteration(iteration => Math.min(iteration + 1, data.length))} title="Next">
-				<Icon type={Icons.chevronRight}/>
-			</button>
-			<button onClick={() => setIteration(iteration => Math.min(iteration + 10, data.length))} title="Skip 10">
-				<Icon type={Icons.chevronsRight}/>
-			</button>
-			<button onClick={() => setIteration(0)} title="Reset">
-				<Icon type={Icons.reset}/>
-			</button>
-		</div>
-
-        <p>{data[iteration].season} - Game Day {data[iteration].gameDay} - {new Date(data[iteration].timestamp).toLocaleDateString()}</p>
-      </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<h1>Goals Racing Bar Chart</h1>
+			<RacingBarChart days={data} iteration={iteration} playerSeasons={seasons}/>
+			<p>{data[iteration].season} - Game
+				Day {data[iteration].gameDay} - {new Date(data[iteration].timestamp).toLocaleDateString()}</p>
+			<div className="buttonContainer">
+				<button onClick={() => setIteration(iteration => Math.max(iteration - 10, 0))} title="Back 10">
+					<Icon type={Icons.chevronsLeft}/>
+				</button>
+				<button onClick={() => setIteration(iteration => Math.max(iteration - 1, 0))} title="Previous">
+					<Icon type={Icons.chevronLeft}/>
+				</button>
+				<button onClick={() => setStart(!start)} title={start ? "Pause" : "Play"}>
+					<Icon type={start ? Icons.pause : Icons.play}/>
+				</button>
+				<button onClick={() => setIteration(iteration => Math.min(iteration + 1, data.length-1))} title="Next">
+					<Icon type={Icons.chevronRight}/>
+				</button>
+				<button onClick={() => setIteration(iteration => Math.min(iteration + 10, data.length-1))}
+						title="Skip 10">
+					<Icon type={Icons.chevronsRight}/>
+				</button>
+				<button onClick={() => {
+					setStart(false);
+					setIteration(0);
+				}} title="Reset">
+					<Icon type={Icons.reset}/>
+				</button>
+			</div>
+		</React.Fragment>
+	);
 }
 
 export default App;
